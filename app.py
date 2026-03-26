94b5f27dbd06b556f26c8a4d079d01522344cb78
aaron <aaroncuesho19@gmail.com>
2026-03-26 01:34:36 -0600

    Implementation of Delete function with confirmation in JavaScript

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Base de datos simulada (Lista de diccionarios)
 catalogo = [
     {"id": 1, "titulo": "Back to the Future", "director": "Robert Zemeckis", "año": 1985},
     {"id": 2, "titulo": "The Matrix", "director": "Wachowskis", "año": 1999}
@@ -10,8 +9,24 @@ catalogo = [
 
 @app.route('/')
 def index():
-    # Mandamos el catálogo al HTML usando Jinja2
     return render_template('index.html', peliculas=catalogo)
+
+# --- NUEVA RUTA PARA CREAR ---
+@app.route('/agregar', methods=['POST'])
+def agregar():
+    # Calculamos un ID nuevo falso sumando 1 al tamaño de la lista
+    nuevo_id = len(catalogo) + 1 
+    # Atrapamos los datos del formulario HTML
+    titulo = request.form['titulo']
+    director = request.form['director']
+    año = request.form['año']
+    
+    # Armamos el nuevo diccionario y lo metemos a la lista
+    nueva_peli = {"id": nuevo_id, "titulo": titulo, "director": director, "año": año}
+    catalogo.append(nueva_peli)
+    
+    # Recargamos la página de inicio para ver los cambios
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
